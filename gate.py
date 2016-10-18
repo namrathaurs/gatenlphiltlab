24604a4d95199da9bcf4c74adaed2eb271d58db6
Nick Barber <nickwbarber@gmail.com>
2016-10-18 11:46:52 -0500

    Began to incorporate ElementTree XML parser

--- gate.py
+++ gate.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 
 import re
+import xml.etree.ElementTree as ET
+
+class InputError(Exception):
+    pass
 
 class GateAnnotation:
     def __init__(self, filename):
         self.filename = filename
 
-    def get_name(self):
-        return self.filename
+    def filename(self, filename):
+        self.filename = filename
 
-    def get_annotations(self, annotation_type, annotation_set):
+    @property
+    def tree(self):
+        return ET.parse(self.filename)
+
+    @property
+    def root(self):
+        return self.tree.getroot()
+
+
+    def get_annotation_sets(self):
+        annotation_sets = []
+        for annotation_set in self.root.iter('AnnotationSet'):
+            annotation_sets.append(annotation_set.get('Name'))
+        return annotation_sets
+
+    # TODO: root.setter if annotation set specified
+
+    def get_annotations(self, annotation_type, *, annotation_set):
         text = open(self.filename).read()
         if annotation_set:
             try:
                 text = re.search(r'<AnnotationSet Name="{}">.*?</AnnotationSet>'.format(annotation_set), text, re.DOTALL).group()
             except AttributeError:
-                print("'{}' is not a valid annotation set".format(annotation_set))
-                quit()
+                raise InputError("'{}' is not a valid annotation set".format(annotation_set))
         return re.findall(r'<Annotation\s+?.*?Type="{}".*?>.*?</Annotation>'.format(annotation_type), text, re.DOTALL)
-    
-    def get_annotation_sets(self):
-        text = open(self.filename).read()
