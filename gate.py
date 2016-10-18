2770f0498b13a0a1c58f9ab2d2d53401d159e6cd
Nick Barber <nickwbarber@gmail.com>
2016-10-18 18:25:58 -0500

    began annotation_kappa function in gate.py

--- gate.py
+++ gate.py
@@ -21,20 +21,20 @@ class GateAnnotation:
     def root(self):
         return self.tree.getroot()
 
+    def get_annotation_set_names(self):
+        return [annotation_set.get('Name') for annotation_set in self.root.iter("AnnotationSet")]
 
-    def get_annotation_sets(self):
-        annotation_sets = []
-        for annotation_set in self.root.iter('AnnotationSet'):
-            annotation_sets.append(annotation_set.get('Name'))
-        return annotation_sets
-
-    # TODO: root.setter if annotation set specified
-
-    def get_annotations(self, annotation_type, *, annotation_set):
-        text = open(self.filename).read()
+    def get_annotations(self, annotation_type, *, annotation_set=None):
         if annotation_set:
-            try:
-                text = re.search(r'<AnnotationSet Name="{}">.*?</AnnotationSet>'.format(annotation_set), text, re.DOTALL).group()
-            except AttributeError:
-                raise InputError("'{}' is not a valid annotation set".format(annotation_set))
-        return re.findall(r'<Annotation\s+?.*?Type="{}".*?>.*?</Annotation>'.format(annotation_type), text, re.DOTALL)
+            return self.root.iterfind(".//AnnotationSet/Annotation[@Type='{}']".format(annotation_type))
+        return self.root.iterfind(".//Annotation[@Type='{}']".format(annotation_type))
+
+def annotation_kappa(annotations1, annotations2, *, schema=None):
+    annotations1_dict = {annotation.get('Type'): annotation for annotation in annotations1}
+    annotations2_dict = {annotation.get('Type'): annotation for annotation in annotations2}
+    annotations = {}
+    for annotation1 in annotations1_dict.keys():
+        for annotation2 in annotations2_dict.keys():
+            if annotations1 == annotations2:
+                annotations.update(annotations1_dict.pop(annotation1), annotations2_dict.pop(annotation2))
+    return annotations
