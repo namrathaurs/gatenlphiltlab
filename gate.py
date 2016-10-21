cb7306801795d73f7f41ec9821dba54418c5d003
Nick Barber <nickwbarber@gmail.com>
2016-10-21 15:59:51 -0500

    pair_annotations() modified to output annotation contents

--- gate.py
+++ gate.py
@@ -33,20 +33,25 @@ class GateAnnotation:
 
 def pair_annotations(annotations1, annotations2):
     annotations1_list, annotations2_list = list(annotations1), list(annotations2)
-    annotation_pairs = {}
+
+    annotation_pairs = []
     for annotation1 in annotations1_list:
         for annotation2 in annotations2_list:
-            if ((int(annotation1.get('StartNode')) >= int(annotation2.get('StartNode'))
-                    and int(annotation1.get('StartNode')) < int(annotation2.get('EndNode')))
-                    or (int(annotation1.get('EndNode')) > int(annotation2.get('StartNode'))
-                        and int(annotation1.get('EndNode')) <= int(annotation2.get('EndNode')))):
-                annotation_pairs.update({annotation1:annotation2})
+            if ( ( int(annotation1.get('StartNode')) >= int(annotation2.get('StartNode'))
+                    and int(annotation1.get('StartNode')) < int(annotation2.get('EndNode')) )
+                    or ( int(annotation1.get('EndNode')) > int(annotation2.get('StartNode'))
+                        and int(annotation1.get('EndNode')) <= int(annotation2.get('EndNode')) ) ):
+                annotation_pairs.append((annotation1, annotation2))
                 annotations2_list.remove(annotation2)
                 break
-    annotations1_list.clear()
-    annotations2_list.clear()
-    return annotation_pairs
+    annotations1_list.clear() and annotations2_list.clear()
 
-def kappa(annotation_pairs):
+    content_pairs = []
     for pair in annotation_pairs:
-        return list(pair), list(annotation_pairs[pair])
+        new_pair = []
+        for annotation in pair:
+            annotation = { feature.findtext('./Name') : feature.findtext('./Value') for feature in list(annotation) }
+            new_pair.append(annotation)
+        content_pairs.append(new_pair)
+
+    return content_pairs
