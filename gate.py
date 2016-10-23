bfdcf9932c76af996066f329da71d3157778e264
Nick Barber <nickwbarber@gmail.com>
2016-10-23 17:33:39 -0500

    First working version of gate.kappa()

--- gate.py
+++ gate.py
@@ -3,9 +3,11 @@
 from collections import namedtuple
 import re
 import xml.etree.ElementTree as ET
+import skll
 
 class InputError(Exception):
     pass
+
 
 class Annotation:
     def __init__(self, filename):
@@ -25,11 +27,12 @@ class Annotation:
     def get_annotation_set_names(self):
         return [annotation_set.get('Name') for annotation_set in self.root.iter("AnnotationSet")]
 
-    def get_annotations(self, annotation_type, *, annotation_set=None):
+    def get_annotations(self, *, annotation_type=None, annotation_set=None):
         if annotation_set:
             return self.root.iterfind(".//AnnotationSet[@Name='{}']/Annotation[@Type='{}']".format(annotation_set, annotation_type))
         else:
             return self.root.iterfind(".//Annotation[@Type='{}']".format(annotation_type))
+
 
 class Schema:
     def __init__(self, filename):
@@ -47,20 +50,19 @@ class Schema:
     def root(self):
         return self.tree.getroot()
 
-    def get_annotations(self, annotation_type):
+    def get_attributes(self, annotation_type):
         attributes = self.root.findall(".//schema:element[@name='{}']//schema:attribute".format(annotation_type), namespaces=self.namespace)
         return attributes
 
 
-
-
-
-def pair_annotations(annotations1, annotations2):
+def pair_annotations(annotations1, annotations2, *, annotation_type=None, schema=None):
     annotations1_list, annotations2_list = list(annotations1), list(annotations2)
 
+    # Build list of annotation pairs
     annotation_pairs = []
     for annotation1 in annotations1_list:
         for annotation2 in annotations2_list:
+            # if annotation spans overlap
             if ( ( int(annotation1.get('StartNode')) >= int(annotation2.get('StartNode'))
                     and int(annotation1.get('StartNode')) < int(annotation2.get('EndNode')) )
                     or ( int(annotation1.get('EndNode')) > int(annotation2.get('StartNode'))
@@ -70,6 +72,7 @@ def pair_annotations(annotations1, annotations2):
                 break
     annotations1_list.clear() and annotations2_list.clear()
 
+    # Unpack Names and Values of each annotation
     content_pairs = []
     for pair in annotation_pairs:
         new_pair = []
@@ -78,6 +81,24 @@ def pair_annotations(annotations1, annotations2):
             new_pair.append(annotation)
         content_pairs.append(new_pair)
 
-    return content_pairs
+    content_pairs = tuple(content_pairs)
 
-#def kappa(content_pairs, schema_file/contents):
+    # Compile comparison sets for each annotation attribute
+    ComparisonSet = namedtuple('ComparisonSet', ['attribute', 'annotator1', 'annotator2'])
+    attributes = [ attribute.get('name') for attribute in schema.get_attributes(annotation_type) ]
+    comparison_sets = []
+    for attribute in attributes:
+        annotator1 = tuple( annotation[0].get(attribute) for annotation in content_pairs )
+        annotator2 = tuple( annotation[1].get(attribute) for annotation in content_pairs )
+        attribute_annotations = ComparisonSet(attribute, annotator1, annotator2)
+        comparison_sets.append(attribute_annotations)
+
+    return comparison_sets
+
+def kappa(comparison_set, weights=None):
+    new_comparison_set = comparison_set
+    new_comparison_set = new_comparison_set._replace(annotator1=tuple( re.sub(r'(\d+).*', r'\1', annotation) for annotation in comparison_set.annotator1 if annotation ))
+    new_comparison_set = new_comparison_set._replace(annotator2=tuple( re.sub(r'(\d+).*', r'\1', annotation) for annotation in comparison_set.annotator2 if annotation ))
+    if (len(new_comparison_set.annotator1) == len(new_comparison_set.annotator2)
+        and not (len(new_comparison_set.annotator1) <= 0 or len(new_comparison_set.annotator2) <= 0) ):
+        return skll.kappa(new_comparison_set.annotator1, new_comparison_set.annotator2, weights=weights)
