4ec1adbb01ee48d8eb73e80d79dd5b3c1e1aaa42
Nick Barber <nickwbarber@gmail.com>
2017-07-31 18:16:55 -0500

    Define main() using hard-coded filepaths

--- f_measure.py
+++ f_measure.py
@@ -109,3 +109,65 @@ def calc_f_measure(key_set,
         # no agreement.
         return 0
 
+def main():
+
+    import gate
+    from functools import reduce
+
+    def get_char_set(gate_annotation):
+        char_set = []
+        head_char_set = set(
+            range(
+                gate_annotation._start_node,
+                gate_annotation._end_node
+            )
+        )
+        char_set.append(head_char_set)
+        if gate_annotation._continuations:
+            continuations = gate_annotation._continuations
+            for x in continuations:
+                continuation_span = set(
+                    range(
+                        x._start_node,
+                        x._end_node
+                    )
+                )
+                char_set.append(continuation_span)
+        char_set = frozenset(
+            reduce( lambda x,y: x | y, char_set )
+        )
+        return char_set
+
+    paths = [
+        "/home/nick/hilt/PES/7/2/4-MG-2014-05-15_PES_2_NB.xml",
+        "/home/nick/hilt/PES/7/2/4-MG-2014-05-15_PES_2_NU.xml"
+    ]
+
+    sets = []
+
+    is_match = is_lenient_match
+
+    for path in paths:
+        annotation_file = gate.AnnotationFile(path)
+        annotations = gate.AnnotationGroup(
+            x for x in annotation_file.iter_annotations()
+        ).get_annotations()
+        spans = []
+        for x in annotations:
+            if x._type == "Attribution":
+                spans.append(get_char_set(x))
+        sets.append(spans)
+
+    key_set = sets[0]
+    response_set = sets[1]
+
+    print(
+        calc_f_measure(
+            key_set,
+            response_set,
+            is_match
+        )
+    )
+
+if __name__ == "__main__":
+    main()
