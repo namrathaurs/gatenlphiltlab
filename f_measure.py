649be0dbb0ec026f9094c85111fdbcb7eec3a111
Nick Barber <nickwbarber@gmail.com>
2017-07-30 21:59:44 -0500

    Begin refactoring to utilize new gate classes

--- f_measure.py
+++ f_measure.py
@@ -75,9 +75,19 @@ def calc_recall(num_true_positives,
 def calc_harmonic_mean( x, y ):
     return 2 * ( ( x * y ) / ( x + y ) )
 
-def calc_f_measure(num_true_positives,
-                   num_false_positives,
-                   num_false_negatives):
+def calc_f_measure(key_set,
+                   response_set,
+                   is_match):
+
+    num_true_positives = sum(
+        1 for _ in iter_true_positives( key_set, response_set, is_match )
+    )
+    num_false_positives = sum(
+        1 for _ in iter_false_positives( key_set, response_set, is_match )
+    )
+    num_false_negatives = sum(
+        1 for _ in iter_false_negatives(key_set, response_set, is_match)
+    )
 
     precision = calc_precision(
         num_true_positives,
@@ -97,30 +107,6 @@ def calc_f_measure(num_true_positives,
         return 0
 
 def main():
-
-    key_set = (
-        frozenset( ( x + 0 for x in range(10) ) ),
-    )
-    response_set = (
-        frozenset( ( x + 0 for x in range(10) ) ),
-        frozenset( ( x + 0 for x in range(10) ) ),
-        frozenset( ( x + 0 for x in range(10) ) ),
-        frozenset( ( x + 0 for x in range(10) ) ),
-        frozenset( ( x + 50 for x in range(10) ) ),
-        frozenset( ( x + 70 for x in range(10) ) ),
-        frozenset( ( x + 90 for x in range(10) ) ),
-        frozenset( ( x + 10 for x in range(10) ) ),
-    )
-
-    num_true_positives = sum(
-        1 for _ in iter_true_positives( key_set, response_set, is_lenient_match )
-    )
-    num_false_positives = sum(
-        1 for _ in iter_false_positives( key_set, response_set, is_lenient_match )
-    )
-    num_false_negatives = sum(
-        1 for _ in iter_false_negatives(key_set, response_set, is_lenient_match)
-    )
 
     print(calc_f_measure(num_true_positives, num_false_positives, num_false_negatives))
 
