712198e8669532b928a1c97da5883c69e200fa80
Nick Barber <nickwbarber@gmail.com>
2019-06-13 10:07:14 -0500

    Remove requirements file; use setuptools instead. Remove superfluous dependencies.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gatenlp',
-    version='3.2',
+    version='3.2.3',
     description='Interface for parsing and manipulating GATE annotation documents',
     url='http://github.com/nickwbarber/gatenlp',
     author='Nick Barber',
@@ -11,8 +11,9 @@ setup(
     license='MIT',
     packages=find_packages(),
     install_requires=[
-        'lxml',
-        'intervaltree'
+        'lxml==4.1.1',
+        'intervaltree==2.1.0',
+        'python-Levenshtein==0.12.0'
     ],
     python_requires='>=3',
     zip_safe=False,
