b2c8611bbe983172d9a80e25d93404e7562dd31c
Nick Barber <nickwbarber@gmail.com>
2019-06-13 09:08:19 -0500

    Fix setup so it installs all subpackages.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -9,7 +9,7 @@ setup(
     author='Nick Barber',
     author_email='nickwbarber@gmail.com',
     license='MIT',
-    packages=['gatenlp'],
+    packages=find_packages(),
     install_requires=[
         'lxml',
         'intervaltree'
